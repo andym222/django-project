9ee6686217956c5d63935a9f1cc35128a00b6119
Andrew Matheson <2209290m@student.gla.ac.uk>
2017-02-02 12:48:10 +0000

    Chapter 6 before exercises

--- rango/views.py
+++ rango/views.py
@@ -1,6 +1,8 @@
+from rango.models import Page
 from django.shortcuts import render
 from django.http import HttpResponse
 from rango.models import Category
+
 
 def index(request):
    
@@ -8,3 +10,30 @@ def index(request):
     context_dict = {'categories': category_list}
       
     return render(request, 'rango/index.html', context_dict)
+
+def show_category(request, category_name_slug):
+ 
+    context_dict = {}
+    
+    try:
+       
+        category = Category.objects.get(slug=category_name_slug)              
+        pages = Page.objects.filter(category=category)               
+        context_dict['pages'] = pages       
+        context_dict['category'] = category
+
+    except Category.DoesNotExist:
+       
+        context_dict['category'] = None
+        context_dict['pages'] = None
+      
+    return render(request, 'rango/category.html', context_dict)    
+        
+def about(request):
+    context = RequestContext(request)
+    
+    if request.session.get('visits'):
+        count = request.session.get('visits')
+    else:
+        count = 0
+    return render_to_response('rango/about.html', {'visits':count}, context)
